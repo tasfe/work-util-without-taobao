436ce800ad0d079158583f894d29aaccdfd81f36
hwy1782@gmail.com <hwy1782@gmail.com@2676e600-654c-8ed8-f1ec-6bc051a9d13c>
2012-12-24 04:47:59 +0000

    放放风

---
+++ src/src/com/hongye/net/Test.java
@@ -0,0 +1,13 @@
+package com.hongye.net;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String str = "Ó¢ ³¬ 12-23";
+		
+		if(str.contains("Ó¢ ³¬")){
+			System.out.println(str);
+		}
+	}
+	
+}
